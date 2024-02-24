64c0f94fd199a1463c7324578efb868195ff6357
caua.ribeiro <caualeu2016@gmail.com>
2024-02-23 21:21:52 -0300

    @Cry199 Update: Arena.java

--- ProjetoTanqueRobo/src/arena/Arena.java
+++ ProjetoTanqueRobo/src/arena/Arena.java
@@ -1,9 +1,17 @@
 package arena;
 
+import roboTanque.CoresRGB;
+import roboTanque.TankRobo;
+
 public class Arena 
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        TankRobo robo1 = new TankRobo(0, 0, CoresRGB.BLUE, CoresRGB.RED, "Robo1");
+        
+        robo1.movNorte();
+        robo1.movLeste();
+    
+        System.out.println(robo1);
     }
 }
